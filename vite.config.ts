fd452bb7a13fd3a76e1eb61fbf6541bc049ed7b5
Eve Lin <207277820+evelinix@users.noreply.github.com>
2025-09-13 14:18:28 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/atlantica-db/'
+  base: "/atlantica-db/",
+  build: {
+    outDir: "docs",
+  },
 })
